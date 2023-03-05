e1fd9d3efbaf956d62a03b8be1bf22ccf0855076
filippopov <filip.popov13@gmail.com>
2023-03-05 21:30:08 +0100

    [1] added modules and dependencies

--- src/main/java/com/example/timskiproekt/TimskiProektApplication.java
+++ src/main/java/com/example/timskiproekt/TimskiProektApplication.java
@@ -1,9 +1,14 @@
 package com.example.timskiproekt;
 
+import com.example.service.ServiceRootConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({
+		ServiceRootConfig.class
+})
 public class TimskiProektApplication {
 
 	public static void main(String[] args) {
