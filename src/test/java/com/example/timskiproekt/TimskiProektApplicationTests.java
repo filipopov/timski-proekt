f9367d9725ff9b644b445d3ab378f18efd21bd6d
filippopov <filip.popov13@gmail.com>
2023-03-05 14:27:12 +0100

    initial commit

---
+++ src/test/java/com/example/timskiproekt/TimskiProektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timskiproekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimskiProektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
