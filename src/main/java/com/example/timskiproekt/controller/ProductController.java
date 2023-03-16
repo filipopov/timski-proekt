d74c50013adc718e01ae8ed22618839cb3693581
filippopov <filip.popov13@gmail.com>
2023-03-16 23:35:13 +0100

    fixed remark

--- src/main/java/com/example/timskiproekt/controller/ProductController.java
+++ src/main/java/com/example/timskiproekt/controller/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
 
